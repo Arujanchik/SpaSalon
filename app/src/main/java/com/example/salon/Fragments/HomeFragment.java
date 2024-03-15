62f475a3aeac3fa917e0309a45729c0535737ee7
Arujanchik <Kozqaras3gmail.com>
2024-03-15 23:28:07 +0500

    Done first service👍

--- app/src/main/java/com/example/salon/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/salon/Fragments/HomeFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.salon.activity.HaircutActivity;
 import com.example.salon.R;
@@ -18,11 +19,20 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-
+        ImageView haircutImageView = view.findViewById(R.id.img_haircut);
+        if (haircutImageView != null) {
+            haircutImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onHaircutClick(v);
+                }
+            });
+        }
         return view;
     }
     public void onHaircutClick(View view) {
-        Intent intent = new Intent(getContext(), HaircutActivity.class);
+        // Здесь код для обработки клика, например, запуск новой активности
+        Intent intent = new Intent(getActivity(), HaircutActivity.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/salon/activity/HaircutActivity.java
+++ app/src/main/java/com/example/salon/activity/HaircutActivity.java
@@ -3,7 +3,9 @@ package com.example.salon.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.salon.Fragments.DescriptionBottomSheetDialogFragment;
 import com.example.salon.R;
 
 public class HaircutActivity extends AppCompatActivity {
@@ -12,5 +14,9 @@ public class HaircutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_haircut);
+    }
+    public void onDescriptionClick(View view) {
+        DescriptionBottomSheetDialogFragment bottomSheet = new DescriptionBottomSheetDialogFragment();
+        bottomSheet.show(getSupportFragmentManager(), "DescriptionBottomSheet");
     }
 }
